acc2482f324a266a797eb4efc9ae3c480a9b8958
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2023-06-27 17:22:53 +0530

    Delete virtual_calculator directory

--- virtual_calculator/p.py
+++
@@ -1,6 +0,0 @@
-buttonListValues = [['7', '8', '9', '*'],
-                    ['4', '5', '6', '-'],
-                    ['1', '2', '3', '+'],
-                    ['0', '/', '.', '=']]
-for i, button in enumerate(buttonList):
-    print(i,button)
--- virtual_calculator/prj1.py
+++
@@ -1,9 +0,0 @@
-
-from cvzone.HandTrackingModule import HandDetector
-detector=HandDetector(detectionCon=0.8,maxHands=1)
-hands, img = detector.findHands(img, flipType=False)   #Hand Detection
-
-
-
-
-
